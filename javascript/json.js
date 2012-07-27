278c08a5a3f5cf66382c8157ae62c1c9f86a5f28
dblock5216@gmail.com <acap10242011>
2012-07-26 21:23:56 -0500

    added json file

---
+++ javascript/json.js
@@ -0,0 +1,31 @@
+// Dorian LAne	
+// VFW 0712
+// July 26th, 2012
+//
+
+var json  = {
+	"cigarette1": {
+		"group": ["Group", "Smoked"],
+		"tday":	["Time of Day", "Noon"],
+		"date": ["Date", "10/24/2011"],
+		"distance": ["Distance", "50"],
+		"childvalue": ["Children", "Yes"],
+		"reason": ["Reason", "bored"],
+	},
+	"cigarette2": {
+		"group": ["Group", "Smoked"],
+		"tday":	["Time of Day", "Morning"],
+		"date": ["Date", "1/31/2011"],
+		"distance": ["Distance", "20"],
+		"childvalue": ["Children", "No"],
+		"reason": ["Reason", "tired"],	
+	},
+	"cigarette3": {
+		"group": ["Group", "Smoked"],
+		"tday":	["Time of Day", "Night"],
+		"date": ["Date", "9/4/2011"],
+		"distance": ["Distance", "10"],
+		"childvalue": ["Children", "Yes"],
+		"reason": ["Reason", "on way to work"],	
+	}
+};
