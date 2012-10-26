1a6c34de0df624cadcd5213e914847793a02089a
dblock5216@gmail.com <acap10242011>
2012-10-25 20:10:38 -0500

    added miu p1 files

---
+++ js/json.js
@@ -0,0 +1,100 @@
+var json = {
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	 "bottle2": {
+		"timeofday": ["Time of Day: ", "Night"],
+		"date": ["Date: ", "22-01-2012"],
+		"ounces": ["Ounces of Milk: ", "12"],
+		"milk": ["Is It Breastmilk?: ", "No"],
+		"notes": ["Notes: ", "Baby only drank half bottle."]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	},
+	"bottle1": {
+		"timeofday": ["Time of Day: ", "Morning"],
+		"date": ["Date: ", "12-06-2012"],
+		"ounces": ["Ounces of Milk: ", "8"],
+		"milk": ["Is It Breastmilk?: ", "Yes"],
+		"notes": ["Notes: ", "None"]
+	}
+}
